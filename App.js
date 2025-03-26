7af1309983f8d5ca80dd4749352e4bb10aa17902
NeoWallDam <sd_nicollas@hotmail.com>
2025-03-26 19:35:44 -0300

    Commit Inicial

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native';
 
 import Home from './src/pages/Home';
-import LEPA from './src/pages/LEPA';
-import Painel from './src/pages/Painel';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,9 +12,7 @@ export default function App(){
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="LEPA" component={LEPA} />
-        <Stack.Screen name="Painel" component={Painel} />
-      </Stack.Navigator>
+        </Stack.Navigator>
     </NavigationContainer>
   )
  }
